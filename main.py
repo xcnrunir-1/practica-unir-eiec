c2f9f0322b00177bf03b3eb538d01e99c4099e48
Xabier Napal Roncal <xcnrunir.1@gmail.com>
2024-04-06 15:27:46 +0200

    traducir todos los textos al ingles

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ DEFAULT_DUPLICATES = False
 
 def sort_list(items, ascending=True):
     if not isinstance(items, list):
-        raise RuntimeError(f"No puede ordenar {type(items)}")
+        raise RuntimeError(f"Can't order {type(items)} type")
 
     return sorted(items, reverse=(not ascending))
 
@@ -28,11 +28,12 @@ if __name__ == "__main__":
         filename = sys.argv[1]
         remove_duplicates = sys.argv[2].lower() == "yes"
     else:
-        print("Se debe indicar el fichero como primer argumento")
-        print("El segundo argumento indica si se quieren eliminar duplicados")
+        print(f"Usage: {sys.argv[0]} [file] [yes|no]")
+        print("File must be specified as first argument.")
+        print("Second argument configures duplicate removal.")
         sys.exit(1)
 
-    print(f"Se leerán las palabras del fichero {filename}")
+    print(f"Reading words from file {filename}")
     file_path = os.path.join(".", filename)
     if os.path.isfile(file_path):
         word_list = []
@@ -40,7 +41,7 @@ if __name__ == "__main__":
             for line in file:
                 word_list.append(line.strip())
     else:
-        print(f"El fichero {filename} no existe")
+        print(f"File {filename} does not exist")
         word_list = ["ravenclaw", "gryffindor", "slytherin", "hufflepuff"]
 
     if remove_duplicates:
